713aca7c5aa0dfd5d06ed53236b6167582e82528
Mario <82867693+MarioTangkas@users.noreply.github.com>
2021-04-25 22:31:58 +0700

    Add files via upload

---
+++ Contact.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <meta charset="utf-8"> 
+        <title>Contact Person</title>
+        <link rel="stylesheet" href="Style.css">
+    </head>
+    <body>
+        <h1>Contact Person</h1>
+    
+    <div class='container'>
+        <img src="instagram.png" class='instagram'>
+        <img src="Whatsappp.png" class='whatsapp'>
+        <img src="email.png" class='email'>
+
+    <form>
+    <textarea>Masukan Pesan anda...</textarea>
+    </form>
+
+    <div class='container2'></div>
+    <img src="kawal.jpeg" class="images1">
+    <h1 class='text1'><?php echo "Kawal Motor" ?> </h1>
+    <h1 class='text2'><?php echo "Berbelanja adalah seni dan saya seorang seniman" ?> </h1>
+    <p class='text3'><?php echo "Opening Hours" ?> </p>
+    <p class='text4'><?php echo "24 hours depend on seller" ?> </p>
+    
+    
+    </body>
+</html>
